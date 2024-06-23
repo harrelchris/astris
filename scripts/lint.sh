6cd4a692788e8a04c67b249d5645167fc61cb308
Chris Harrel <harrelchris@gmail.com>
2024-06-23 18:13:54 -0500

    Add src_path

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,4 +6,4 @@ black app --line-length 120 --extend-exclude='^(.*/)?(migrations|fixtures)/'
 
 flake8 app --max-line-length 120 --extend-exclude .idea/,.venv/,*/migrations/
 
-isort app --profile black --line-length 120
+isort app --profile black --line-length 120 --src-path app
